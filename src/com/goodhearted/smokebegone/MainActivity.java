509f1283a426fdf1a2acbc94490932990a812419
Nick Gobee <nickgobee0122@gmail.com>
2013-06-07 10:47:05 +0200

    Test commit 2

--- src/com/goodhearted/smokebegone/MainActivity.java
+++ src/com/goodhearted/smokebegone/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        int a = 2 + 3;
+        String text = "Hello";
     }
 }
