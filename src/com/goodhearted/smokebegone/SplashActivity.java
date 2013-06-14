39742a23f54b6ee9c3e62872d97f65b568ec106f
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-06-14 11:47:41 +0200

    Splashactivity now only showing once

--- src/com/goodhearted/smokebegone/SplashActivity.java
+++ src/com/goodhearted/smokebegone/SplashActivity.java
@@ -4,28 +4,34 @@ package com.goodhearted.smokebegone;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.Window;
 
 public class SplashActivity extends Activity {
 
+	static boolean already_ran = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
-		Thread timer = new Thread(){
-			public void run() {
-				try {
-					sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+		if(already_ran) {
+			startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+		} else {
+			Thread timer = new Thread(){
+				public void run() {
+					try {
+						sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+					}
 				}
-			}
-		};
-		timer.start();
+			};
+			already_ran = true;
+			timer.start();
+		}
 	}
 
 	@Override
