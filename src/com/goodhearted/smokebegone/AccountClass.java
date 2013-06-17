553b8e988f7e7b5f1d9898992562724475c91338
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-06-17 20:54:37 +0200

    Some work on the DB model

--- src/com/goodhearted/smokebegone/AccountClass.java
+++ src/com/goodhearted/smokebegone/AccountClass.java
@@ -1,31 +1,43 @@
 package com.goodhearted.smokebegone;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.preference.PreferenceManager;
 
-public class AccountClass extends SQLiteOpenHelper {
+public final class AccountClass extends SQLiteOpenHelper{
 
+	public static final String TABLE_SMOKES = "smokes";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_DATE = "dat";
+	
+	public static final String DATABASE_NAME = "smokebegone.db";
+	public static final int DATABASE_VERSION = 1;
+	
+	private static final String SMOKES_CREATE = "create table "
+		      + TABLE_SMOKES + "(" + COLUMN_ID
+		      + " integer primary key autoincrement, " + COLUMN_DATE
+		      + " DATETIME not null);";
+	
+	SharedPreferences settings;
+	
 	public AccountClass(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(SMOKES_CREATE);
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
-		
-	}
+	  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	    db.execSQL("DROP TABLE IF EXISTS " + TABLE_SMOKES);
+	    onCreate(db);
+	  }
 
-	
 }
+
