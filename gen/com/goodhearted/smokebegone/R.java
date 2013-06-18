0ccf457755394b6a06975d8c71811a15e01044ce
Richard Portegies <richard_portegies@hotmail.com>
2013-06-18 14:03:02 +0200

    info doneer button

--- gen/com/goodhearted/smokebegone/R.java
+++ gen/com/goodhearted/smokebegone/R.java
@@ -24,21 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
-        public static final int benefits=0x7f08000c;
-        public static final int doneren=0x7f08000d;
-        public static final int hoofdscherm=0x7f08000b;
-        public static final int info=0x7f080008;
-        public static final int minusbutton=0x7f080007;
-        public static final int motivatie=0x7f08000a;
-        public static final int plusbutton=0x7f080006;
-        public static final int settings=0x7f080009;
+        public static final int action_settings=0x7f08000f;
+        public static final int benefits=0x7f08000d;
+        public static final int button1=0x7f080003;
+        public static final int doneren=0x7f08000e;
+        public static final int hoofdscherm=0x7f08000c;
+        public static final int info=0x7f080009;
+        public static final int minusbutton=0x7f080008;
+        public static final int motivatie=0x7f08000b;
+        public static final int plusbutton=0x7f080007;
+        public static final int settings=0x7f08000a;
         public static final int tab1=0x7f080001;
-        public static final int tab2=0x7f080003;
+        public static final int tab2=0x7f080004;
         public static final int tabHost=0x7f080000;
-        public static final int tvSMOKES=0x7f080005;
+        public static final int tvSMOKES=0x7f080006;
         public static final int txt1=0x7f080002;
-        public static final int txt2=0x7f080004;
+        public static final int txt2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
