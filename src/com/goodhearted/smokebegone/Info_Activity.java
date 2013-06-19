d91bb85207cf9c528f1744f39949764bc66eea5a
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-06-19 11:54:51 +0200

    Fixing menuinflater

--- src/com/goodhearted/smokebegone/Info_Activity.java
+++ src/com/goodhearted/smokebegone/Info_Activity.java
@@ -45,14 +45,6 @@ public class Info_Activity extends Activity {
 			}
 		});}
 			
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.info_, menu);
-		return true;
-	}
 	
 	public boolean onOptionsItemSelected(MenuItem item){
         SlideHolder x = (SlideHolder) findViewById(R.id.bla);
