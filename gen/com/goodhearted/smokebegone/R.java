aa6b0ccf78f34b081e95e53ff89451c15179ffab
Richard Portegies <richard_portegies@hotmail.com>
2013-06-19 11:57:17 +0200

    Info text, Button

--- gen/com/goodhearted/smokebegone/R.java
+++ gen/com/goodhearted/smokebegone/R.java
@@ -75,6 +75,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int donate_button=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int info_kwf=0x7f05000b;
         public static final int title_activity_info=0x7f050004;
