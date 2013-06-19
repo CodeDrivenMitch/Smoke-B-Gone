8fabeab2e75b2652ff2b73a223c8d7cb8a110aaa
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-06-19 09:56:42 +0200

    Now toggable by title bar

--- gen/com/goodhearted/smokebegone/R.java
+++ gen/com/goodhearted/smokebegone/R.java
@@ -24,29 +24,34 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f08001d;
         public static final int bSave=0x7f080003;
-        public static final int benefits=0x7f08000f;
-        public static final int doneren=0x7f080010;
-        public static final int etNumPerPack=0x7f080012;
-        public static final int etPrice=0x7f080013;
-        public static final int etsmokesperDay=0x7f080017;
-        public static final int hoofdscherm=0x7f08000e;
-        public static final int info=0x7f080009;
-        public static final int minusbutton=0x7f080008;
-        public static final int motivatie=0x7f08000d;
-        public static final int plusbutton=0x7f080007;
-        public static final int settings=0x7f08000c;
+        public static final int benefits=0x7f080011;
+        public static final int bla=0x7f080006;
+        public static final int button1=0x7f08001a;
+        public static final int button2=0x7f08001b;
+        public static final int button3=0x7f08001c;
+        public static final int doneren=0x7f080012;
+        public static final int etNumPerPack=0x7f080014;
+        public static final int etPrice=0x7f080015;
+        public static final int etsmokesperDay=0x7f080019;
+        public static final int hoofdscherm=0x7f080010;
+        public static final int id=0x7f080007;
+        public static final int info=0x7f08000b;
+        public static final int minusbutton=0x7f08000a;
+        public static final int motivatie=0x7f08000f;
+        public static final int plusbutton=0x7f080009;
+        public static final int settings=0x7f08000e;
         public static final int tab1=0x7f080001;
         public static final int tab2=0x7f080004;
         public static final int tabHost=0x7f080000;
-        public static final int textView1=0x7f080011;
-        public static final int textView2=0x7f080014;
-        public static final int textView3=0x7f080015;
-        public static final int textView4=0x7f080016;
-        public static final int tvDATE=0x7f08000a;
-        public static final int tvSINCE=0x7f08000b;
-        public static final int tvSMOKES=0x7f080006;
+        public static final int textView1=0x7f080013;
+        public static final int textView2=0x7f080016;
+        public static final int textView3=0x7f080017;
+        public static final int textView4=0x7f080018;
+        public static final int tvDATE=0x7f08000c;
+        public static final int tvSINCE=0x7f08000d;
+        public static final int tvSMOKES=0x7f080008;
         public static final int txt1=0x7f080002;
         public static final int txt2=0x7f080005;
     }
@@ -56,6 +61,7 @@ public final class R {
         public static final int activity_menu=0x7f030002;
         public static final int activity_splash=0x7f030003;
         public static final int activity_welcome=0x7f030004;
+        public static final int slidemenulayout=0x7f030005;
     }
     public static final class menu {
         public static final int info=0x7f070000;
--- src/com/goodhearted/smokebegone/MainActivity.java
+++ src/com/goodhearted/smokebegone/MainActivity.java
@@ -2,12 +2,12 @@ package com.goodhearted.smokebegone;
 
 import java.util.Date;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,7 +20,9 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
         setContentView(R.layout.activity_main);
         DAO = new SmokeDataSource(this);
         plus = (Button) findViewById(R.id.plusbutton);
@@ -34,6 +36,13 @@ public class MainActivity extends Activity implements OnClickListener {
         tvsince = (TextView) findViewById(R.id.tvSINCE);
         updateTV();
         updateTVDATE();
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item){
+        SlideHolder x = (SlideHolder) findViewById(R.id.bla);
+        x.toggle();
+        return true;
+
     }
 
 	@Override
