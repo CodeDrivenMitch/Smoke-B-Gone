e8e3494046b78b359058b518597cf0bde4d382ab
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-06-24 20:43:38 +0200

    Layout width fix

--- src/com/goodhearted/smokebegone/MainActivity.java
+++ src/com/goodhearted/smokebegone/MainActivity.java
@@ -11,8 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
